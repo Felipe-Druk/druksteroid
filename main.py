5453f893aecfc80bbddd6810e7c95b4441e9d8ea
Felipe-Druk <fedalto@fi.uba.ar>
2026-02-22 13:39:51 -0300

    feat: new persitence featur for save high score and player name

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from game import Game
 from menu_druksteroid import MenuDruksteroid
+from persistence.high_score_savior import HighScoreSavior, HighScore
 
 def main():
     pygame.init()
@@ -14,6 +15,9 @@ def main():
 
     game = Game()
     score = 0
+    high_score = HighScoreSavior("tall_score.pickle", HighScore(score, "DRUK") )
+    high_old = high_score.deserialize()
+    print(high_old.score)
     menu = MenuDruksteroid(game.screen)
 
     while True:
@@ -23,6 +27,8 @@ def main():
 
         if score < game.get_score():
             score = game.get_score()
+            high_score = HighScoreSavior("tall_score.pickle", HighScore(score, "DRUK") )
+            high_score.serialize()
             print(f"New high score: {score}")
         
         game.reset()
---
+++ persistence/savior.py
@@ -0,0 +1,19 @@
+import pickle
+
+
+
+class Savior:
+
+    def __init__(self, file_name):
+        self._datos = None
+        self._file_name = file_name
+
+    def serialize(self):
+        with open(self._file_name, 'wb') as f:
+            pickle.dump(self._datos, f)
+
+    def deserialize(self):
+        with open(self._file_name, 'rb') as f:
+            loded_data = pickle.load(f)
+            return loded_data
+        return None
